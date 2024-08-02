b333cafc86af118b342e14cd72ba9bbe2a255d9f
nereazj544 <nzjvirtual@gmail.com>
2024-08-02 18:16:39 +0200

    d

--- javascript/YT/js/archivo.js
+++ javascript/YT/js/archivo.js
@@ -1,2 +1,20 @@
-document.write('hola perras')
-document.write('🤌')
+// document.write('hola perras')
+
+
+// var nombre; 
+// alert('INSERTE SU NOMBRE')
+// nombre = prompt('NOMBRE') 
+// document.write("Hola, " + nombre);
+// document.write('<br>')
+// let orta;
+// alert('INSERTA ALGO')
+// orta = prompt('NO SÉ');
+// document.write(otra)
+
+var p = {nombre: neo, edad:19}
+document.write(p.nombre)
+
+var Persona = {nombre: neo, edad: 19};
+document.write(Persona.nombre)
+
+
